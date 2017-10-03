047c2ba811e76adf4f82076dafac4268dce1fb46
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-03 17:07:49 -0400

    Add route for /favourites, displays favourite_links view

--- server.rb
+++ server.rb
@@ -11,3 +11,15 @@ end
 get "/about_me" do
   erb :about
 end
+
+get "/favourites" do
+  @fav_links = [
+    "http://www.linkylink1.com",
+    "http://www.linkylink2.com",
+    "http://www.linkylink3.com",
+    "http://www.linkylink4.com",
+    "http://www.linkylink5.com"
+  ]
+
+  erb :favourite_links
+end
